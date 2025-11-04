cec58e37c369f26effc19d416cd5c56cb4e9d9ba
LuisaTu2 <luisa.pegoraro@gmail.com>
2025-11-04 12:10:23 -0500

    [isa] new build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -40,9 +40,9 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        entryFileNames: `assets/index.js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
       },
     },
   },
