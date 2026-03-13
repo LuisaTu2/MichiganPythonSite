03bff6939dc9ffbae3bc9a1647a269af9ae63d35
LuisaTu2 <luisa.pegoraro@gmail.com>
2026-03-13 16:06:08 -0400

    [isa] made debug and secret key environment vars

--- MichiganPythonSite/settings.py
+++ MichiganPythonSite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from os.path import join
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,15 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ruy!_k@iu_kb4d3wtxa9)h!vs2$6eyhr^=!y^6%a3(rbkd^4iv"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['luisaanywhere.pythonanywhere.com', '127.0.0.1','localhost']
+ALLOWED_HOSTS = ["luisaanywhere.pythonanywhere.com", "127.0.0.1", "localhost"]
 
 # Application definition
 
